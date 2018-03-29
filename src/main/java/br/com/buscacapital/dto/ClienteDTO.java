26e758cfee614f2998aeaecaeec118a7389c3280
BBPF0415 - Yanisley Mora Ritchie <BBPF0415 - Yanisley Mora Ritchie@BBPGETECK1.bbp.com.br>
2018-03-29 16:01:03 -0300

    Criação de DTO para retono de webservice

---
+++ src/main/java/br/com/buscacapital/dto/ClienteDTO.java
@@ -0,0 +1,129 @@
+package br.com.buscacapital.dto;
+
+import javax.persistence.Column;
+
+public class ClienteDTO {
+
+	private String uf;
+	
+	private String cidade;
+	
+	private String cep;
+	
+	private String bairro;
+	
+	private String logradouro;
+
+	private Integer numero;
+	
+	private String complemento;
+
+	private String tipo;
+	
+	private String email;
+
+	private String telefone;
+	
+	private String nome;
+	
+	private String cpfCnpj;
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	
+	
+}
--- src/main/java/br/com/buscacapital/ws/ClienteWs.java
+++ src/main/java/br/com/buscacapital/ws/ClienteWs.java
@@ -17,7 +17,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import br.com.buscacapital.bo.ClienteBO;
+import br.com.buscacapital.dto.ClienteDTO;
 import br.com.buscacapital.model.Cliente;
+import br.com.buscacapital.model.ClientePf;
+import br.com.buscacapital.model.ClientePj;
 
 @Controller
 public class ClienteWs {
@@ -34,12 +37,50 @@ public class ClienteWs {
 		
 		List<Cliente> listaCliente = new ArrayList<Cliente>(this.clienteBO.listarTodos());
 		
-		String objRetorno = gsonCliente.toJson(listaCliente);
+		List<ClienteDTO> listaRetorno = this.trataListaCliente(listaCliente);
+		
+		String objRetorno = gsonCliente.toJson(listaRetorno);
 		
 		
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
 		
 		return new ResponseEntity<String>(objRetorno, responseHeaders, HttpStatus.OK);
+	}
+
+	/**
+	 * 
+	 * @param listaCliente
+	 * @return
+	 */
+	private List<ClienteDTO> trataListaCliente(List<Cliente> listaCliente) {
+		List<ClienteDTO> listaRetorno = new ArrayList<ClienteDTO>();
+		
+		if (!listaCliente.isEmpty()) {
+			for (Cliente cliente : listaCliente) {
+				ClienteDTO cDto = new ClienteDTO();
+				
+				cDto.setTipo(cliente.getTipoCliente());
+				cDto.setEmail(cliente.getEmail());
+				
+				cDto.setBairro(cliente.getEnderecoBairro());
+				cDto.setCep(cliente.getEnderecoCep());
+				cDto.setCidade(cliente.getEnderecoCidade());
+				cDto.setComplemento(cliente.getEnderecoComplemento());
+				cDto.setLogradouro(cliente.getEnderecoLogradouro());
+				cDto.setUf(cliente.getEnderecoUf());
+				cDto.setNumero(cliente.getEnderecoNumero());
+				
+				cDto.setTelefone(cliente.getTelefone());
+				cDto.setCpfCnpj(cliente.getTipoCliente().equalsIgnoreCase("PF") ? ((ClientePf) cliente).getCpf() : ((ClientePj) cliente).getCnpj());
+				cDto.setNome(cliente.getTipoCliente().equalsIgnoreCase("PF") ? ((ClientePf) cliente).getNomeCompleto() : ((ClientePj) cliente).getNomeFantasia());
+				
+				listaRetorno.add(cDto);
+			}
+			
+			
+		}
+		
+		return listaRetorno;
 	}
 }
